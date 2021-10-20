14fb8f7005678069598d9a92ffb9b4254b9a0abc
vk5-pdx <vk5@pdx.edu>
2021-10-19 21:57:25 -0700

    calculating data and code size from number of parity bits

--- hdl/hamming_encoder.sv
+++ hdl/hamming_encoder.sv
@@ -14,18 +14,27 @@
 
 module ham_encoder
 #(
-	parameter WIDTH = 7;
+	parameter PARITY_BITS = 3;
 
-	// TODO : calculate this in code
-	parameter CODE_WIDTH = 11;
-
-	// do not specify during instantiation
-	//parameter CODE_WIDTH = $clog2(WIDTH+CODE_WIDTH+1)
+	// do not edit these
+	parameter DATA_WIDTH = 2**PARITY_BITS - 1;
+	parameter CODE_WIDTH = 2**PARITY_BITS - PARITY_BITS -1;
 )
 (
 	input  logic [WIDTH:1]    data,
 	output logic [CODE_WIDTH:1] ham_code
 );
+
+// verifying if user entered parameters
+localparam CALC_DATA_WIDTH = 2**PARITY_BITS - 1;
+localparam CALC_CODE_WIDTH = 2**PARITY_BITS - PARITY_BITS -1;
+
+generate if(CALC_DATA_WIDTH != DATA_WIDTH)
+	$fatal("Wrong DATA_WIDTH specified for module ham_encoder");
+endgenerate
+generate if(CALC_CODE_WIDTH != CODE_WIDTH)
+	$fatal("Wrong CODE_WIDTH specified for module ham_encoder");
+endgenerate
 
 logic parity_bits = 0;
 logic [CODE_WIDTH-1] parity_bit_mask = 1;
